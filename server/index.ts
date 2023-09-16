d08dc8f3d5552720e8b02a0ddec3e80b3cacf8b7
Muhammad-bin-ali <muhammadbinali706@gmail.com>
2023-09-16 17:35:47 -0400

    WIP. Not working but trying

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,10 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// const ConnectDB = require("./db/conn");
+
+// import db from "./db/conn";
 
 //For env File
 dotenv.config();
@@ -11,6 +16,24 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Express & TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
-});
+const connString =
+  "mongodb+srv://HackMIT2023:WCtlrWURq9DzZna7@hackmit-cluster.4p9wi1v.mongodb.net/?retryWrites=true&w=majority";
+
+async function ConnectDb() {
+  await mongoose.connect(connString);
+}
+
+const start = async () => {
+  try {
+    await mongoose.connect(connString);
+    app.listen(port, () => console.log("Server started on port 3000"));
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+start();
+// app.listen(port, () => {
+//   console.log(`Server is Fire at http://localhost:${port}`);
+// });
