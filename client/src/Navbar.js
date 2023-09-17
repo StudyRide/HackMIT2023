6b6b1129e52ca2aba86cdcebbe8d56a18c6fd700
daniel <dmallett@vols.utk.edu>
2023-09-17 02:08:10 -0400

    styling changes

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -6,32 +6,35 @@ import React, { useState } from 'react';
 
 function Navbar() {
 
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  // const [isDropdownOpen, setDropdownOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setDropdownOpen(!isDropdownOpen);
-  };
+  // const toggleDropdown = () => {
+  //   setDropdownOpen(!isDropdownOpen);
+  // };
 
   return (
     <nav className="navbar">
       <div className="logo">
-        <span>StudyRide</span>
+        <a href="/">
+          <span>StudyRide</span>
+        </a>
       </div>
       <ul className="nav-links">
         <li><a href="/">About Us</a></li>
         <li><a href="/">Log In</a></li>
         <li><a href="/">Sign Up</a></li>
+        <li><a href="/dashboard">Dashboard</a></li>
         {/* When we add log in functionality, it's gonna be one or the other */}
-        <li onClick={toggleDropdown}>
+        {/* <li onClick={toggleDropdown}>
           <span>Profile</span>
           {isDropdownOpen && (
             <ul>
-              <li><a href="#item1">Dropdown Item 1</a></li>
+              <li><a href="/dashboard">Dashboard</a></li>
               <li><a href="#item2">Dropdown Item 2</a></li>
               <li><a href="#item3">Dropdown Item 3</a></li>
             </ul>
           )}
-        </li>
+        </li> */}
       </ul>
     </nav>
   );
