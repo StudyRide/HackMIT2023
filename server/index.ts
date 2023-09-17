c385e7c9eb6487311e76568feaae7a4e5ab08850
madkate42 <edinorog42@yandex.ru>
2023-09-17 02:27:35 -0400

    a bit better

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response, Application } from "express";
 import mongoose from "mongoose";
 import { User } from "./db/models/User";
 import { Ride } from "./db/models/Ride";
+import router from "./routes";
 
 const cors = require('cors');
 const app: Application = express();
@@ -11,84 +12,7 @@ const connString =
   "mongodb+srv://ebondare:eZWqUiDAyhltnqfC@studyride-cluster.c55hbb6.mongodb.net/";
 
 //eZWqUiDAyhltnqfC
-
-app.post("/createUser", (req: Request, res: Response) => {
-  console.log(req.query);
-  const newUser = new User({
-    name: req.query.name,
-    email: req.query.email,
-    phoneNumber: req.query.phoneNumber,
-    confirmedRides: []
-  });
-  newUser.save()
-    .then(() => res.json('User created!'))
-    // @ts-ignore
-    .catch((err) => res.status(400).json('Error: ' + err))
-});
-
-app.get("/", (req: Request, res: Response) => {
-  res.send(
-    "Good evening!"
-  );
-});
-
-const items = [
-  {
-    name: "Laptop",
-    price: 500,
-  },
-  {
-    name: "Desktop",
-    price: 700,
-  },
-];
-
-app.get("/api/items", (req, res) => {
-  res.send(items);
-});
-
-app.get('/getRides', (req: Request, res: Response) => {
-  console.log("/getRides called")
-  Ride.find({})
-    .then((rides) => {
-        res.json(rides);
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send('An error occurred while trying to get rides');
-    });
-});
-
-app.get('/getUsers', (req: Request, res: Response) => {
-  User.find({})
-    .then((rides) => {
-        res.json(rides);
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send('An error occurred while trying to get userss');
-    });
-})
-
-
-app.post('/createRide', (req, res) => {
-  const newRide = new Ride({
-      driver: req.query.driver,
-      capacity: req.query.capacity,
-      origin: req.query.origin,
-      destination: req.query.destination,
-      timeOfCreation: new Date(),
-      rideDepartureTime: req.query.rideDepartureTime,
-      rideReturnTime: req.query.rideReturnTime,
-      cost: req.query.cost,
-      requests: []
-  });
-  newRide.save()
-      .then(() => res.json('Ride created!'))
-      // @ts-ignore
-      .catch((err) => res.status(400).json('Error: ' + err));
-});
-
+app.use("/", router);
 
 const start = async () => {
   try {
@@ -101,19 +25,3 @@ const start = async () => {
 };
 
 start();
-
-// console.log(
-//   User.create({
-//     email: "hi@gmail.com",
-//     name: "thisIsAName",
-//     phoneNumber: "123.456.7890",
-//   }).then((res) => {
-//     res;
-//   }),
-
-//   User.find({})
-//     .exec()
-//     .then((res) => {
-//       console.log(res);
-//     })
-// );
