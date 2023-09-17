d028a6df586520e4aa34b0d40d26835e47798db5
madkate42 <edinorog42@yandex.ru>
2023-09-17 01:51:53 -0400

    Fixed Fetch

--- client/src/Main_Container_1.js
+++ client/src/Main_Container_1.js
@@ -2,14 +2,34 @@
 
 import React from 'react';
 import './Main_Container_1.css'; // Import the CSS file for styling
+import { useEffect, useState } from "react"
 
 function Main_Container_1() {
+  const [rides, setRides] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4242/getRides', {
+      method: "GET",
+      mode: "cors"
+    })
+      .then((res) => res.json())
+      .then((data) => setRides(data));
+  }, []);
+
   return (
     <div className="main-container-1">
       <div className="centered-content">
         <h2 className="search-hook">Find Your Next Shared Ride!</h2>
         <input type="text" className="search-bar" placeholder="Search for a ride" />
         <button className="start-ride-button">Start</button>
+      </div>
+      <div>
+      {rides.map((ride) => (
+          <div key={ride.id}>
+            {/* Output the entire ride object */}
+            {JSON.stringify(ride.origin)}
+          </div>
+        ))}
       </div>
     </div>
   );
