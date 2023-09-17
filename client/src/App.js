4344543560f611f11e372edd1d7af5d650791740
Kristiana Karshelieva <kris.krsh02@gmail.com>
2023-09-16 21:24:51 -0400

    Main Page Updates

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Main_Container_1 />
+      <PersonDash />
       <div className = "rides">
         <RidesComponent/>
       </div>
